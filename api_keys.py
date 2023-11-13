8bbb9477fbff83cdf66c8f21d881d9012c1f384a
Sophie-GJ <gj.sophie@gmail.com>
2023-11-13 17:22:17 -0600

    prepared for part 1
    up to req 1

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e334aae3d8d0fc9465b663b6a740605f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "Y56e8f115de154c6caf81c6cca6c75ca7"
